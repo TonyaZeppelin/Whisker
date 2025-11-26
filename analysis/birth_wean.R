5332584c9a139a1615aa89ee76716131f1b483e4
Amanda.Warlick <amanda.warlick@akcsm-cd4791736.nmfs.local>
2025-11-26 09:24:13 -0800

    New pup ('p') and age-2 ('j') growth rate tables.

--- analysis/birth_wean.R
+++ analysis/birth_wean.R
@@ -16,7 +16,7 @@ dat <- dat_proc %>% filter(length_class=="long") %>% arrange(id,segment_percenti
 
 # Locations where first derivative is zero - output from long_analysis.R
 zeros <- read.csv("results/id_zeros_Brost.csv")
-head(zeros)
+# head(zeros)
 
 # Harvest dates
 harvest <- read.csv("data/raw/all.csv")
@@ -59,40 +59,42 @@ dat_sum <- dat_sum %>% left_join(harvest,by="id")
 dat_sum <- dat_sum %>% mutate(id=as.numeric(id)) %>% arrange(id) %>%
 	rename(birth_percentile=segment_percentile)
 
-# Birth date assuming 90 days in utero growth
-diu <- 90  # assumed number of days whisker growth in utero 
-birth_90 <- dat_sum %>% 
-	mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
-	# mutate(birth_date=collection_date-(max_percentile-birth_percentile)*(diu/birth_percentile)) %>%
-	mutate(age=collection_date-birth_date,
-		   in_utero_growth_rate=birth_percentile*whisker_length/diu,
-		   post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) %>%
-	select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
-		   in_utero_growth_rate,post_utero_growth_rate) %>%
-	mutate(across(where(is.double),\(x) round(x,2))) %>%
-	mutate(age=round(age))	   
-
-# Birth date assuming 120 days in utero growth
-diu <- 120  # assumed number of days whisker growth in utero 
-birth_120 <- dat_sum %>% 
-	mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
-	mutate(age=collection_date-birth_date,
-		   in_utero_growth_rate=birth_percentile*whisker_length/diu,
-		   post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) %>%
-	select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
-		   in_utero_growth_rate,post_utero_growth_rate) %>%
-	mutate(across(where(is.double),\(x) round(x,2))) %>%
-	mutate(age=round(age))	 
+# # Birth date assuming 90 days in utero growth
+# diu <- 90  # assumed number of days whisker growth in utero 
+# birth_90 <- dat_sum %>% 
+# 	mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
+# 	# mutate(birth_date=collection_date-(max_percentile-birth_percentile)*(diu/birth_percentile)) %>%
+# 	mutate(age=collection_date-birth_date,
+# 		   in_utero_growth_rate=birth_percentile*whisker_length/diu,
+# 		   post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) %>%
+# 	select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
+# 		   in_utero_growth_rate,post_utero_growth_rate) %>%
+# 	mutate(across(where(is.double),\(x) round(x,2))) %>%
+# 	mutate(age=round(age))	   
+# 
+# # Birth date assuming 120 days in utero growth
+# diu <- 120  # assumed number of days whisker growth in utero 
+# birth_120 <- dat_sum %>% 
+# 	mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
+# 	mutate(age=collection_date-birth_date,
+# 		   in_utero_growth_rate=birth_percentile*whisker_length/diu,
+# 		   post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) %>%
+# 	select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
+# 		   in_utero_growth_rate,post_utero_growth_rate) %>%
+# 	mutate(across(where(is.double),\(x) round(x,2))) %>%
+# 	mutate(age=round(age))	 
 
 #ajw addition June 2025
 #assume mean birth date of July 9th(?)
 diu <- 120  # assumed number of days whisker growth in utero 
-birth_mean_120 <- dat_sum %>% 
+birth_mean_120p <- dat_sum %>% 
   # mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
   mutate(birth_date = as.Date("2015-07-09")) %>%
   mutate(age=collection_date-birth_date,
          in_utero_growth_rate=birth_percentile*whisker_length/diu,
-         post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) %>%
+         # post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) 
+        #why dividing by diu for post_utero?
+        post_utero_growth_rate=(1-birth_percentile)*whisker_length/(as.numeric(age))) %>%
   
   select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
          in_utero_growth_rate,post_utero_growth_rate) %>%
@@ -100,25 +102,107 @@ birth_mean_120 <- dat_sum %>%
   mutate(age=round(age))
 
 diu <- 90  # assumed number of days whisker growth in utero 
-birth_mean_90 <- dat_sum %>% 
+birth_mean_90p <- dat_sum %>% 
   # mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
   mutate(birth_date = as.Date("2015-07-09")) %>%
   mutate(age=collection_date-birth_date,
          in_utero_growth_rate=birth_percentile*whisker_length/diu,
-         post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu) %>%
-  
+         # post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu
+         #why dividing by diu for post_utero?
+         post_utero_growth_rate=(1-birth_percentile)*whisker_length/(as.numeric(age))) %>% #birth to collection
+
   select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
          in_utero_growth_rate,post_utero_growth_rate) %>%
   mutate(across(where(is.double),\(x) round(x,2))) %>%
   mutate(age=round(age))
 
 # Write output
-write.csv(birth_90,"results/birth_dates_90.csv",row.names=FALSE)
-write.csv(birth_120,"results/birth_dates_120.csv",row.names=FALSE)
+# write.csv(birth_90,"results/birth_dates_90.csv",row.names=FALSE)
+# write.csv(birth_120,"results/birth_dates_120.csv",row.names=FALSE)
 
-write.csv(birth_mean_90,"results/birth_dates_mean_90.csv",row.names=FALSE)
-write.csv(birth_mean_120,"results/birth_dates_mean_120.csv",row.names=FALSE)
+write.csv(birth_mean_90p,"results/birth_dates_mean_90p.csv",row.names=FALSE)
+write.csv(birth_mean_120p,"results/birth_dates_mean_120p.csv",row.names=FALSE)
 
+######age-2
+ggplot(dat %>% filter(id %in% c('T17', 'T3', 'T5')),
+                      aes(x=segment_percentile,y=d15N)) + 
+  facet_wrap(~id) +
+  geom_line() + 
+  geom_point() +
+  geom_vline(data=zeros %>% filter(isotope=="d15N",age_n==2 & segment_percentile < 0.3) %>% group_by(id) %>% filter(fit==min(fit)),
+             aes(xintercept=segment_percentile),col=2)
+
+# ggplot(dat %>% filter(id %in% c('T17', 'T3', 'T5')),
+#        aes(x=segment_percentile,y=d13C)) + 
+#   facet_wrap(~id) +
+#   geom_line() + 
+#   geom_point() +
+#   geom_vline(data=zeros %>% filter(isotope=="d13C",age_n==2 & segment_percentile < 0.3) %>% group_by(id) %>% filter(fit==min(fit)),
+#              aes(xintercept=segment_percentile),col=2)
+
+dat_sum2 <- dat %>% filter(age_n=="2",id!=13) %>% 
+  # filter(segment_percentile < 0.3) %>%
+  group_by(id) %>% 
+  summarize(max_percentile=max(segment_percentile),
+            whisker_length=max(segment_end))
+
+# Add birth dip
+dat_sum2 <- dat_sum2 %>% 
+  left_join(
+    zeros %>% filter(isotope=="d15N",age_n==2) %>% select(id,segment_percentile,fit) %>%
+      filter(segment_percentile < 0.3) %>%
+      group_by(id) %>% filter(fit==min(fit)),  # minimum fitted value is birth dip
+    by="id")
+
+dat_wean <- dat_sum2 %>% 
+  left_join(
+    zeros %>% filter(isotope=="d15N",age_n==2) %>% select(id,segment_percentile,fit) %>%
+      filter(segment_percentile < 0.52 & segment_percentile > 0.22) %>%
+      group_by(id) %>% filter(fit==max(fit)),  # minimum fitted value is birth dip
+    by="id")
+
+# Add harvest date	
+dat_sum2 <- dat_sum2 %>% left_join(harvest,by="id") %>% left_join(dat_wean, by = c('id', 'whisker_length'))
+
+# Tidy data
+dat_sum2 <- dat_sum2 %>% #mutate(id=as.numeric(id)) %>% arrange(id) %>%
+  rename(birth_percentile=segment_percentile) #%>%
+  # mutate(id=as.numeric(id))
+
+#ajw addition June, November 2025
+#assume mean birth date of July 9th
+diu <- 120  # assumed number of days whisker growth in utero 
+birth_mean_120 <- dat_sum2 %>% 
+  # mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
+  mutate(birth_date = as.Date("2012-07-09")) %>%
+  #growth rates
+  mutate(age=collection_date-birth_date,
+         in_utero_growth_rate=birth_percentile*whisker_length/diu,
+         # post_utero_growth_rate=(1-birth_percentile)*whisker_length/diu, #birth to collection
+         #why dividing by diu for post_utero?
+         post_utero_growth_rate=(1-birth_percentile)*whisker_length/(as.numeric(age))) %>% #birth to collection
+
+  #growth rate from birth to wean?
+  
+  select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
+         in_utero_growth_rate,post_utero_growth_rate) %>%
+  mutate(across(where(is.double),\(x) round(x,2))) %>%
+  mutate(age=round(age))
+
+diu <- 90  # assumed number of days whisker growth in utero 
+birth_mean_90 <- dat_sum2 %>% 
+  # mutate(birth_date=collection_date-(1-birth_percentile)*(diu/birth_percentile)) %>%
+  mutate(birth_date = as.Date("2012-07-09")) %>%
+  mutate(age=collection_date-birth_date,
+         in_utero_growth_rate=birth_percentile*whisker_length/diu,
+         post_utero_growth_rate=(1-birth_percentile)*whisker_length/(as.numeric(age))) %>%
+  select(id,birth_percentile,collection_date,age,whisker_length,birth_date,
+         in_utero_growth_rate,post_utero_growth_rate) %>%
+  mutate(across(where(is.double),\(x) round(x,2))) %>%
+  mutate(age=round(age))
+
+write.csv(birth_mean_90,"results/birth_dates_mean_90j.csv",row.names=FALSE)
+write.csv(birth_mean_120,"results/birth_dates_mean_120j.csv",row.names=FALSE)
 
 # Old code...
 # birth <- zeros %>% filter(isotope=="d15N",age_n==0,id!=13) %>% 
